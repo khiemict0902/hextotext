2aa56c8cc3c0f9d5a23ad16c9b1a75d0fa0fd0dd
khiemict0902 <94027281+khiemict0902@users.noreply.github.com>
2022-02-15 20:07:42 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+function myFunction() {
+    var x = document.getElementById("input").value;
+    var y = hexToAscii(x);
+    document.getElementById("demo").innerHTML = y;
+
+
+//write function convert hexadecimal to ASCII format
+function hexToAscii(hex) {
+    var str = '';
+    for (var i = 0; i < hex.length; i += 2)
+        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+    return str;
+}
+
+
+
+}
+
+let myText = document.getElementById("input");
+let myBtn = document.getElementById("myBtn");
+
+
+    myText.addEventListener("keyup", e => {
+        e.preventDefault();
+        if (e.key === "Enter") {
+            myBtn.click();
+        }
+
+        
+    })
+    myBtn.addEventListener("click", e => {
+        myFunction();
+    })
+        
