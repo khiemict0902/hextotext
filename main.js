d643b5e4940e1a07a3aac7d26f94717e3c476e51
khiemict0902 <94027281+khiemict0902@users.noreply.github.com>
2022-03-14 10:05:12 +0700

    test 1

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ function myFunction() {
     var y = hexToAscii(x);
     document.getElementById("demo").innerHTML = y;
 
-//write function convert hexadecimal to ASCII format
-function hexToAscii(hex) {
-    var str = '';
-    for (var i = 0; i < hex.length; i += 2)
-        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
-    return str;
-}
+    //write function convert hexadecimal to ASCII format
+    function hexToAscii(hex) {
+        var str = '';
+        for (var i = 0; i < hex.length; i += 2)
+            str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+        return str;
+    }
 }
 
 let myText = document.getElementById("input");
@@ -19,11 +19,10 @@ myText.addEventListener("keyup", e => {
         e.preventDefault();
         if (e.key === "Enter") {
             myBtn.click();
-        }
-
-        
+        }   
     })
+
 myBtn.addEventListener("click", e => {
         myFunction();
     })
-        
+     
